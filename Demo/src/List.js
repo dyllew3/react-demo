e464b6e8e9ee49e23955b2c99a80678f5d7a1aa0
dyllew3 <lewisdy@tcd.ie>
2020-03-08 22:18:01 +0000

    updates

--- Demo/src/List.js
+++ Demo/src/List.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export class List extends React.Component {
 
-   constructor(props) {
+  constructor(props) {
     super(props);
     // change code below this line
-this.state = {
-  userInput: '',
-  toDoList: []
-};
+    this.state = {
+      userInput: '',
+      toDoList: []
+    };
     // change code above this line
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -28,22 +28,23 @@ this.state = {
     });
   }
   render() {
-    const items = this.state.toDoList.map(function(item){
-      return <li className="list-group-item list-group-item-primary" > {item} </li>;
+    // Generate items
+    const items = this.state.toDoList.map(function (item) {
+      return <li className="list-group-item list-group-item-primary" onClick={() => alert("Deleted")}> {item} </li>;
     });
     return (
       <div>
-        <h1 className="navbar navbar-expand-lg navbar-light bg-light">My "To Do" List:</h1>
+        <h1 className="navbar navbar-expand-lg navbar-light bg-light">My List: {this.props.title}</h1>
         <br />
-        
+
         <textarea
           onChange={this.handleChange}
           value={this.state.userInput}
-          placeholder="Separate Items With Commas" /><br />
-          <button onClick={this.handleSubmit}>Add to list</button>
-          <br />
+          placeholder="Enter item here" /><br />
+        <button onClick={this.handleSubmit}>Add to list</button>
+        <br />
 
-<ul className="list-group">
+        <ul className="list-group">
           {items}
         </ul>
       </div>
